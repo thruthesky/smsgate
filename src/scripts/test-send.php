fffdf8f7649f3ccc707456f476b0653fc6579793
JaeHo Song <thruthesky@gmail.com>
2015-07-20 22:01:21 +0900

    Updating smsgate server

--- src/scripts/test-send.php
+++ src/scripts/test-send.php
@@ -1,28 +1,39 @@
 <?php
 use Drupal\Core\Http\Client;
 
+
 $url_server = "http://sonub.org";
+$username = 'canary';
+$password = 'canary';
+
 $url_server = "http://dev.withcenter.com";
 $username = 'thruthesky';
 $password = 'asdf99';
 
+
+
+$numbers = [
+    '09174678603',
+    '09067104270',
+    '09057210344',
+    '09265581264',
+];
+
 $client = new Client();
 
-for ( $i = 0; $i < 9 ; $i ++) {
-    $number = "09054776789";
-    $message = "D Message No. $i";
-    $url = "$url_server/smsgate/send?username=$username&password=$password&number=$number&message=$message";
-    $response = $client->post($url, [], ['verify'=>false]);
-    $code = $response->getStatusCode();
-    $re = $response->json();
-    //$re = json_decode($re,true);
-    // print_r($re);
-
-    if ( isset($re['error']) && $re['error'] < 0 ) {
-        echo "ERROR ($re[error]) : $re[message]\n";
-    }
-    else {
-        echo "SUCCESS - $number : $message\n";
+for ( $i = 0; $i < 10 ; $i ++) {
+    foreach( $numbers as $number) {
+        $message = "F Message No. $i";
+        $url = "$url_server/smsgate/send?username=$username&password=$password&number=$number&message=$message";
+        $response = $client->post($url, [], ['verify'=>false]);
+        $code = $response->getStatusCode();
+        $re = $response->json();
+        if ( isset($re['error']) && $re['error'] < 0 ) {
+            echo "ERROR ($re[error]) : $re[message]\n";
+        }
+        else {
+            echo "SUCCESS - $number : $message\n";
+        }
     }
 }
 
