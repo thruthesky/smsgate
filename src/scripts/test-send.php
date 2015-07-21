ea2066680c40f370d9a0f2adea326cbfc972f6c1
JaeHo Song <thruthesky@gmail.com>
2015-07-21 22:39:41 +0900

    Updating message and SMS

--- src/scripts/test-send.php
+++ src/scripts/test-send.php
@@ -16,13 +16,15 @@ $numbers = [
     '09174678603',
     '09067104270',
     '09057210344',
+    '0935-1123-877', // galaxy ace
+    '0918-393-2954', // king kom
 ];
 
 $client = new Client();
 
 for ( $i = 0; $i < 10 ; $i ++) {
     foreach( $numbers as $number) {
-        $message = "G Message No. $i";
+        $message = "L Message No. $i";
         $url = "$url_server/smsgate/send?username=$username&password=$password&number=$number&message=$message";
         $response = $client->post($url, [], ['verify'=>false]);
         $code = $response->getStatusCode();
